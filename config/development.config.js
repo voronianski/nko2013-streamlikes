481fa568c5a2b17445d7e2643e7269aea4502c62
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-09 17:45:49 +0200

    add api uris

--- config/development.config.js
+++ config/development.config.js
@@ -2,14 +2,16 @@ module.exports = {
 	url: 'http://0.0.0.0:8000',
 	mongo: {
 		connection: 'mongodb://localhost:27017/nkotracks?auto_reconnect=true',
-		collections: ['users']
+		collections: ['users', 'music']
 	},
 	services: {
 		facebook: {
+			apiUrl: 'https://graph.facebook.com',
 			appID: '586399898092997',
 			appSecret: '486bfb8a132799ae9d3140f261b669d7'
 		},
 		shuffler: {
+			apiUrl: 'https://api.shuffler.fm/v2',
 			appKey: 'b3rsurohdv',
 			appSecret: 'pgn1u9ih9jt1ley0wnxrbgmt1w657cglf4z9qa3qm'
 		}
--- config/production.config.js
+++ config/production.config.js
@@ -2,14 +2,16 @@ module.exports = {
 	url: 'http://kosmetika.2013.nodeknockout.com',
 	mongo: {
 		connection: 'mongodb://ksmtk:hjvtj99@paulo.mongohq.com:10073/nkotracks?auto_reconnect=true',
-		collections: ['users']
+		collections: ['users', 'music']
 	},
 	services: {
 		facebook: {
+			apiUrl: 'https://graph.facebook.com',
 			appID: '732610503419963',
 			appSecret: '75a62db88fc24f483ff3ea94fa45c56a'
 		},
 		shuffler: {
+			apiUrl: 'https://api.shuffler.fm/v2',
 			appKey: 'b3rsurohdv',
 			appSecret: 'pgn1u9ih9jt1ley0wnxrbgmt1w657cglf4z9qa3qm'
 		}
