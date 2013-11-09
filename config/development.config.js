d629579dc3afd267f182cd485ae500f4f6470c60
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-09 13:57:22 +0200

    add env agnostic configs

---
+++ config/development.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	mongo: {
+		connection: 'mongodb://localhost:27017/nkotracks',
+		collections: [],
+		options: { auto_reconnect: true }
+	},
+	services: {}
+};
---
+++ config/production.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	mongo: {
+		connection: '',
+		collections: [],
+		options: { auto_reconnect: true }
+	},
+	services: {}
+};
